6749456a4ed28a3f31770f4eda5fa4801c8f4f2b
Diego França <diego.franca92@gmail.com>
2023-11-24 22:10:03 -0300

    filter button selected style

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,11 +2,12 @@ import { useState, useEffect } from 'react'
 import api from "../services/api";
 import { Pagination } from "../components/Pagination";
 import { NavBar } from "../components/NavBar";
-
+import closeIcon from '../assets/close.svg'
 
 export default function Home() {
 
   const [movieGenre, setMovieGenre] = useState<Movie.IGenre[]>([])
+  const [filterSelected, setFilterSelected] = useState<boolean>(false)
 
   async function getGenres() {
     try {
@@ -15,6 +16,20 @@ export default function Home() {
     } catch (e) {
       console.log(e);
     }
+  }
+
+  function handleFilterGenre(item: string) {
+    const selectedFiltered: string[] = []
+    
+    if (!selectedFiltered.includes(item)) {
+      selectedFiltered.push(item)
+      setFilterSelected(!filterSelected)
+      let selectedItem = document.getElementById(item)
+      selectedItem?.classList.toggle('bg-selected')
+      selectedItem?.classList.toggle('text-white')
+      selectedItem?.firstElementChild?.classList.toggle('hidden')
+    }
+    
   }
 
   useEffect(() => {
@@ -35,7 +50,11 @@ export default function Home() {
           <h5 className="uppercase mb-4">Filtrar por:</h5>
           <div className="flex gap-4 flex-wrap justify-center">
             {movieGenre?.map(category => (
-              <button key={category.name} className="bg-white rounded-sm px-4 p-1 text-black hover:bg-gray-200">{category.name}</button>
+              <button onClick={() => handleFilterGenre(category.id.toString())} id={category.id.toString()} key={category.id} 
+              className={`rounded-sm px-4 p-1 bg-white text-black flex items-center`}
+              >
+                {category.name} <img src={closeIcon} className='w-[16px] h-[16px] ms-2 hidden' />
+              </button>
             ))}
           </div>
         </section>
